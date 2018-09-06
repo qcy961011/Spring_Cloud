8824e1f5aea917f1ea3203027e79f475c754c98c
qcy961011 <18568437879@163.com>
2018-09-07 00:21:01 +0800

    增加ribbon & feign 的hystrix 熔断器

---
+++ hystrix-feign-service/src/main/java/com/qiao/hystrixfeignservice/HystrixFeignServiceApplication.java
@@ -0,0 +1,18 @@
+package com.qiao.hystrixfeignservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+public class HystrixFeignServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixFeignServiceApplication.class, args);
+    }
+}
